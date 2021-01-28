fdbf9ab08200ed20515d4bdb44a6f2026fd78410
syz247179876 <247179876@qq.com>
2021-01-28 14:02:16 +0800

    go切片进阶

--- 06slice/main.go
+++ 06slice/main.go
@@ -34,4 +34,49 @@ func main() {
 
 	// make函数创建切片
 
+	w1 := make([]int, 5, 10)
+	fmt.Printf("w1=%v len(w1)=%d cap(s1)=%d\n", w1, len(w1), cap(w1))
+
+	// 切片遍历
+	for i := 0; i < len(s2); i++ {
+		fmt.Println(s2[i])
+	}
+	for i, v := range s2 {
+		fmt.Println(i, v)
+	}
+
+	// 追加元素,进行扩容
+	z1 := []string{"江苏", "上海", "深圳"}
+	fmt.Printf("z1=%v len(z1)=%d cap(z1)=%d\n", z1, len(z1), len(z1))
+	z1 = append(z1, "广州") // 要用原来的切片
+	fmt.Printf("z1=%v len(z1)=%d cap(z1)=%d\n", z1, len(z1), len(z1))
+
+	z1 = append(z1, "杭州", "南京")
+
+	ss := []string{"武汉", "成都"}
+	z1 = append(z1, ss...) // ...表示拆开
+	fmt.Printf("z1=%v len(z1)=%d cap(z1)=%d\n", z1, len(z1), len(z1))
+
+	// copy
+
+	k1 := []int{1, 3, 5}
+	k2 := k1 // 赋值， 相当于浅拷贝
+	k3 := make([]int, 3, 3)
+	copy(k3, k1) // 相当于深拷贝
+	fmt.Println(k1, k2, k3)
+	k1[0] = 2
+	fmt.Println(k1, k2, k3)
+
+	// 删除切片元素, 指针指向
+	x1 := [...]int{1, 3, 5, 7, 9, 11}
+	x2 := x1[:] // 构造一个切片
+	x2 = append(x2[:1], x2[4:]...)
+	fmt.Println(x1)
+
+	var e = make([]int, 5, 10)
+	for i := 0; i < 10; i++ {
+		e = append(e, i)
+	}
+	fmt.Println(e)
+
 }
