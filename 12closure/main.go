b7d5d0b6267d806e1cd1775e2a967c00ff2450a5
syz247179876 <247179876@qq.com>
2021-01-29 16:33:20 +0800

    go的闭包

--- 12closure/main.go
+++ 12closure/main.go
@@ -28,7 +28,25 @@ func f4(f func(int, int), m, n int) func() {
 	return temp
 }
 
+// 闭包引用
+func calculate(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		base += i
+		return base
+	}
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	return add, sub
+}
+
 func main() {
 	//f3(f2, 3,4)()
 	f1(f4(f2, 4, 3)) //7
+
+	f1, f2 := calculate(10)
+	fmt.Println(f1(1), f2(3)) // 11, 8
+	fmt.Println(f1(3), f2(4)) // 11, 7
+
 }
