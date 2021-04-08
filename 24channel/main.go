e41344e42dc6a4fa8a264999b82784023f391353
syz247179876 <247179876@qq.com>
2021-04-08 16:00:59 +0800

    学习channel和goroutine

--- 24channel/main.go
+++ 24channel/main.go
@@ -23,22 +23,12 @@ func createChan() {
 
 	fmt.Println(x)
 	fmt.Println("你好")
+	close(channel1)
 
 	//fmt.Println(channel2, channel1, channel3)
 }
 
-func recv(c chan int) {
-	ret := <-c // 如果没有发送值过来则会阻塞
-	fmt.Println("接受成功", ret)
-}
-
-func main() {
-	// ch := make(chan int)   // 定义一个无缓冲通道,即同步通道
-	//ch2 := make(chan int, 1) // 创建一个容量为1的有缓冲区通道
-	//go recv(ch2)
-	//// ch <- 10
-	//fmt.Println("发送成功")
-
+func rangeFor() {
 	// 开启一个goroutine定义一个匿名函数，发送值到ch1
 	ch1 := make(chan int)
 	ch2 := make(chan int)
@@ -65,4 +55,52 @@ func main() {
 	for i := range ch2 { // 如果通道关闭，则会退出for range操作
 		fmt.Println(i)
 	}
+}
+
+func recv(c chan int) { // c为双向通道
+	ret := <-c // 如果没有发送值过来则会阻塞
+	fmt.Println("接受成功", ret)
+}
+
+// 写通道
+func writer(in chan<- int) {
+	for i := 1; i < 100; i++ {
+		in <- i
+	}
+	close(in)
+}
+
+// 读写通道
+func wr(in chan<- int, out <-chan int) {
+	for i := range out {
+		in <- i * i
+	}
+	close(in)
+}
+
+// 读通道
+func read(out <-chan int) {
+	for i := range out {
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	ch := make(chan int)     // 定义一个无缓冲通道,即同步通道
+	ch2 := make(chan int, 1) // 创建一个容量为1的有缓冲区通道
+	go recv(ch)
+	ch <- 10
+	go recv(ch2)
+	fmt.Println("发送成功")
+
+	/* -----------------for range------------------- */
+
+	// rangeFor()  // 使用for range 遍历channel
+
+	/* ----------------单向通道---------------------- */
+	inner := make(chan int)
+	outer := make(chan int)
+	go writer(inner)    // 向通道中写数据
+	go wr(outer, inner) // 遍历inner，写入outer
+	read(outer)         // 遍历outer
 }
